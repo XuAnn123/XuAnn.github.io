9373ace4595553ef754009c6330d29fb77028dff
XuAnn123 <44332343+XuAnn123@users.noreply.github.com>
2019-04-07 19:13:58 +0800

    Update 迷宮.cpp

--- zerojudge/迷宮.cpp
+++ zerojudge/迷宮.cpp
@@ -1,47 +1,43 @@
-#include<cstring> 
-#include<iostream> 
-#include<queue>
+#include<iostream>
+#include<queue> 
+#include<cstring>
 using namespace std;
-int n,step[105][105];
-char arr[105][105];
-//BFS
 struct node{
 int x,y;
 };
-
 int main(){
-int dx[4] = {1,-1,0,0} , dy[4] = {0,0,1,-1};
-//x[0]y[0] 下降一格 x[1]y[1] 上升一格 x[2]y[2] 往右一格 x[3]y[3] 往左一格
-queue<node> q;
-memset(step,0,sizeof(step));
-cin>>n;	
-for(int i=1;i<=n;i++)
-	for(int j=1;j<=n;j++){
-		cin>>arr[i][j];	
-		if(arr[i][j]=='#')	step[i][j] = 0;
-	}
-
-step[2][2]=1;
-q.push( (node){2,2} );
-
-while(!q.empty()){
-	for(int k=0;k<4;k++){ //控制上下左右 
+char arr[105][105];
+int step[105][105];
+int x[4]={1,-1, 0,0};
+int y[4]={0, 0,-1,1};
+int n;
+while(cin>>n){
+	memset(step,0,sizeof(step));
+	for(int i=1;i<=n;i++)	
+		for(int j=1;j<=n;j++)
+			cin>>arr[i][j];
+	step[2][2] = 1;
+	queue <node> q;
+	q.push((node){2,2});
+	while(!q.empty()){
 		node temp = q.front();
-		int xx = temp.x , yy = temp.y;  	
+		int xx = temp.x, yy = temp.y;
 		q.pop();
-		if(arr[xx][yy] == '.') {	
-			step[xx][yy] = step[xx+dx[k]][xx+dx[k]] + 1;
-			q.push( (node){xx+dx[k],xx+dy[k]} );
-			arr[xx+dx[k]][xx+dx[k]] = '#';
-		}
-		}
-}	
-
-for(int i=1;i<=n;i++){
-	for(int j=1;j<=n;j++)
-		cout<<step[i][j]<<" ";
-	cout<<endl;
+		for(int i=0;i<4;i++){
+			if(arr[xx+x[i]][yy+y[i]] == '.'){
+				step[xx+x[i]][yy+y[i]] = step[xx][yy] + 1;
+				q.push( (node) {(xx+x[i]),(yy+y[i])});
+				arr[xx+x[i]][yy+y[i]] = '#';	
+			}
+		}			
+	}
+	/*for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++)
+			cout<<step[i][j]<<" ";
+		cout<<endl;
+	}*/
+	if(step[n-1][n-1] == 0) cout<<"No solution!"<<endl;
+	else cout<<step[n-1][n-1]<<endl;
 }
-		
-return 0;
+return 0;		
 }
